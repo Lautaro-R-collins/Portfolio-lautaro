ff587d124116a2aade963c284f9720bb45212c66
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-07-13 18:46:25 -0300

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: 'http://localhost:4321/',
+  integrations: [react(), mdx(), sitemap(), tailwind()]
+});
---
+++ src/components/Type.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+export const Type = () => {
+  return (
+    <>
+      <Typewriter
+        options={{
+          strings: ["Developer", "Frontend", "Backend", "Freelancer"],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 25
+        }}
+      />
+    </>
+  );
+};
+
+export default Typewriter;
