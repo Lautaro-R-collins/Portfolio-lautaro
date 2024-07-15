f25207f9245e5ff53466e9c7735f045c38e3e61e
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-07-15 20:36:13 -0300

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	  extend: {},
+		extend: {},
 	},
-	darkMode: 'class', // Utiliza el modo oscuro basado en clases
 	fontFamily: {
-	  poppins: ['Poppins', 'sans-serif'],
-	},
-	plugins: [
-	  require('@tailwindcss/typography'),
-	  require('daisyui')
-	],
+        poppins: ['Poppins', 'sans-serif'],
+    },
+	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-	  themes: false, // Utiliza solo los temas light + dark
-	  darkTheme: 'dark', // Nombre del tema oscuro que quieres utilizar
-	  logs: false,
-	},
-  };
+		themes: ["dark"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: "dark", // name of one of the included themes for dark mode
+		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+	  }
+}
