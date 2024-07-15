20a3641d387656d680e1642442025daf55dab39f
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-07-15 20:38:29 -0300

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
 		themes: ["dark"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		darkTheme: "false", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
